c3ca326e3a4cd6843daa8314a11c96af746fafab
Ursula Aseye Amoaku <ursula.aseye@gmail.com>
2026-04-18 22:42:32 +0000

    Fixes

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -90,11 +90,11 @@ STORAGES = {
         'BACKEND': 'cloudinary_storage.storage.MediaCloudinaryStorage',
     },
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
     },
 }
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompresseStaticFilesStorage'
 
 CLOUDINARY_STORAGE = {
     'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL'),
